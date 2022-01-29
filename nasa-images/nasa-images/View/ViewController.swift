e40922fad58f9b28afa196282a35b515562f8ee1
Gabriela Sillis <gsillis@Gabrielas-MacBook-Pro.local>
2022-01-29 13:14:09 -0300

    feature: initial setup

---
+++ nasa-images/nasa-images/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nasa-images
+//
+//  Created by Gabriela Sillis on 29/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
