501ee81c8bdbcaa7797475f10854156596add4d7
Gabriela Sillis <gsillis@Gabrielas-MacBook-Pro.local>
2022-02-21 20:16:36 -0300

    feature: test service layer

--- nasa-images/nasa-imagesTests/ServiceLayerTest/ServiceLayerTest.swift
+++ nasa-images/nasa-imagesTests/ServiceLayerTest/ServiceLayerTest.swift
@@ -3,40 +3,68 @@
 //  nasa-imagesTests
 //
 //  Created by Gabriela Sillis on 29/01/22.
-//
+// swiftlint:disable line_length
 
 import XCTest
 @testable import nasa_images
 
 class ServiceLayerTest: XCTestCase {
-	
-	func test_should_complete_with_correct_url_and_method() {
-		let url = makeURL()
-		testRequestFor(url: makeURL()) { request  in
-			XCTAssertEqual(url, request.url)
-			XCTAssertEqual("GET", request.httpMethod)
-		}
-	}
+    
+    func test_should_complete_with_correct_url_and_method() {
+        let url = makeURL()
+        testRequestFor(url: makeURL()) { request  in
+            XCTAssertEqual(url, request.url)
+            XCTAssertEqual("GET", request.httpMethod)
+        }
+    }
+
+    func test_should_complete_with_success_when_request_completes_with_200() {
+        expectResult(.success(makeValidData()), when: (makeValidData(), makeResponse(), nil))
+    }
+
+    func test_should_complete_with_failure_when_request_completes_with_non_200() {
+        expectResult(.failure(.badRequest), when: (makeValidData(), makeResponse(statusCode: 400), nil))
+    }
 }
 
 extension ServiceLayerTest {
-	func makeSut(file: StaticString = #filePath, line: UInt = #line) -> ServiceLayer {
-		let configuration = URLSessionConfiguration.default
-		configuration.protocolClasses = [URLProtocolStub.self]
-		let session = URLSession(configuration: configuration)
-		let sut = ServiceLayer(urlSession: session)
-		testMemoryLeak(instance: sut, file: file, line: line)
-		return sut
-	}
-	
-	func testRequestFor(url: URL, completion: @escaping (URLRequest) -> Void ) {
-		let sut = makeSut()
-		sut.fetchAstronomyImages(url: url) { _ in }
-		let exp = expectation(description: "waiting")
-		URLProtocolStub.observerRequest { request in
-			completion(request)
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1)
-	}
+    typealias StubTuple = (data: Data?, response: HTTPURLResponse?, error: Error?)
+    
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> ServiceLayer {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = ServiceLayer(urlSession: session)
+        testMemoryLeak(instance: sut, file: file, line: line)
+        return sut
+    }
+    
+    func testRequestFor(url: URL, completion: @escaping (URLRequest) -> Void) {
+        let sut = makeSut()
+        let exp = expectation(description: "waiting")
+        sut.fetchAstronomyImages(url: url) { _ in exp.fulfill() }
+        var request: URLRequest?
+        URLProtocolStub.observerRequest { request = $0 }
+        wait(for: [exp], timeout: 1)
+        completion(request!)
+    }
+    
+    func expectResult(_ expectedResult: (Result<Data, NetworkError>), when stub: StubTuple, file: StaticString = #filePath, line: UInt = #line) {
+        let sut = makeSut()
+        URLProtocolStub.simulatedata(data: stub.data, response: stub.response, error: stub.error)
+        
+        let exp = expectation(description: "waiting")
+        sut.fetchAstronomyImages(url: makeURL()) { receivedResult in
+            switch (expectedResult, receivedResult) {
+            case (.success(let expSuccess), .success(let receveidSuccess)):
+                XCTAssertEqual(expSuccess, receveidSuccess, file: file, line: line)
+            case (.failure(let expError), .failure(let receveidError)):
+                XCTAssertEqual(expError, receveidError, file: file, line: line)
+            default:
+                XCTFail("Expected \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
 }
--- nasa-images/nasa-imagesTests/ServiceLayerTest/URLProtocolStub.swift
+++ nasa-images/nasa-imagesTests/ServiceLayerTest/URLProtocolStub.swift
@@ -10,10 +10,19 @@ import Foundation
 // Stub 
 class URLProtocolStub: URLProtocol {
 	static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var error: Error?
+    static var response: HTTPURLResponse?
 	
 	static func observerRequest(completion: @escaping (URLRequest) -> Void) {
 		URLProtocolStub.emit = completion
 	}
+    
+    static func simulatedata(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        self.data = data
+        self.error = error
+        self.response = response
+    }
 	
 	override open class func canInit(with request: URLRequest) -> Bool {
 		return true
@@ -25,6 +34,19 @@ class URLProtocolStub: URLProtocol {
 	
 	override open func startLoading() {
 		URLProtocolStub.emit?(request)
+        
+        if let data = URLProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = URLProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let error = URLProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self)
 	}
 	
 	override open func stopLoading() {}
